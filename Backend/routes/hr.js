a6b38952c1994ac6be71faa417ea9d4ef6dbea79
jishnu <jishnukrishnan99@gmail.com>
2021-07-26 21:16:59 +0530

    fixed

--- Backend/routes/hr.js
+++ Backend/routes/hr.js
@@ -52,35 +52,6 @@ router.post('/register/', function(req, res, next) {
     })
 });
 
-// @desc login url for normal user 
-// @route get /users/login/
-// router.all('/login/', (req, res, next) => {
-//     const name =req.body.username;
-//     const password = req.body.password;
-//     Company.getCompanyByName(name, (err, company)=> {
-//         if(!company){
-//             return res.json({success: false, msg: 'Company Name not found'});
-//         }else{
-//             Company.comparePassword(password, company.password, (err, isMatch) => {
-//                 if(err) throw err;
-//                 if(isMatch){
-//                     const token = jwt.sign(company.toJSON(), process.env.JWT_KEY, {
-//                         expiresIn: 604800 // one week
-//                     });
-//                     res.json({
-//                         success: true, token: 'JWT '+token,
-//                         company:{
-//                             id: company._id,
-//                             name: company.name
-//                         }
-//                     });
-//                 }else{
-//                     return res.json({success: false, msg: 'Wrong Password'});
-//                 }
-//             });
-//         }
-//     })
-// });
 
 // @desc Job posting api
 // @route post /hr/post/create/
